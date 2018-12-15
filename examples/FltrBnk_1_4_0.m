8de07ca433aae9896898a76d7e7cf9010309a230
Kenneth Martin <martin@granitesemi.com>
2018-12-15 08:04:12 -0500

    Modified and added examples, such as for Filter Banks, and a small overview file (EXAMPLES.txt)

---
+++ examples/FltrBnk_1_4_0.m
@@ -0,0 +1,47 @@
+% The first two filters are analog (continuous-time) filters.
+
+% A symetric filter that should be real
+N = 256;
+delta_f = 1/N;
+%w_shift = pi*j;
+w_shift = 0.0j;
+p = [-0.2 -0.1 0.1 0.2]; % initial guess at finite loss poles
+ni=1; % number of loss poles at infinity
+wp = []; ws = [];
+wp(1) = -delta_f/2; % lower passband edge
+wp(2) = delta_f/2; % upper passband edge
+ws = [-0.49 -0.049 0.049 0.49];
+as = [70 50 50 70];
+Ap = 1.05; % the passband ripple in dB
+px = [];
+ONE_STP = 0;
+% A positive-pass continuous-time filter with a elliptic pass-band
+
+svSpecs = {p, px, wp, ws};
+
+H1 = dsgnDigitalFltr(p, px, ni, wp, ws, as, Ap, 'monotonic');
+plot_drsps(H1, wp, ws, 'b', [-0.5 0.5 -180 1]);
+cscdFltr1 = mkCscdFltrD(H1, wp);
+%cscdFltr1.plotGn(wp, ws, -100, 2);
+xin = zeros(8192,1);
+xin(1) = 1;
+ylim = [-180 2];
+
+tic
+Out = simCscdFltrBnk(cscdFltr1, xin, delta_f);
+toc
+
+tic
+hndl = figure('Position',[800 100 600 600]);
+[ax1 ax2, f, ymRef] = plotRspns(Out(:,1), [-0.05 0.1], 'b', ylim);
+hold(ax1, 'on');
+hold(ax2, 'on');
+sum = Out(:,1);
+for i = 2:N
+    plotRspns(Out(:,i), [-0.5 0.5], 'b', ylim);
+    sum = sum + Out(:,i);
+end
+plotRspns(sum, [-0.5 0.5], 'r', ylim);
+toc
+
+a=1;
